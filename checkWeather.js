7fbc29eab45a6ebc6946cbeb14748586b6da1372
Shamreen Tabassum <shamreenstudy1@gmail.com>
2022-04-22 14:04:24 +0200

    checkWeather.js

---
+++ checkWeather.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser")
+const app = express();
+
+app.use(bodyParser.urlencoded({extented:true}));
+
+
+app.get("/", function(req,res)
+{
+  res.sendFile(__dirname + "/index.html"); });
+
+app.post("/", function(req,res)
+{
+  const query = req.body.city;
+  const apiKey = "1cfb4537d3410f11c96fb5465228eaa3";
+  const unit = "metric";
+  const url ="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units="+ unit;
+
+  https.get(url,function(response){
+      console.log(response.statusCode);
+
+      response.on("data", function(data)
+     {
+       const weatherData = JSON.parse(data);
+       const temperature = weatherData.main.temp;
+       const weatherDescription = weatherData.weather[0].description;
+       const icons = weatherData.weather[0].icon;
+       const imageURL = "http://openweathermap.org/img/wn/"+ icons +"@2x.png";
+       res.write("<h1>The current temperature is " + temperature + " degree Celsius </h1><br><h1>The weather description is " + weatherDescription + "</h1>");
+       res.write("<img src=" + imageURL + ">");
+       res.send();
+   });
+
+  });
+
+});
+
+
+app.listen(3000, function(){
+  console.log("server is running at port 3000");
+});
